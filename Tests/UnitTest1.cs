6d029332f6f5b87c8dbbd12018b3c23a0843895f
Kleber Miranda Lima <klebermirandalima@gmail.com>
2024-02-03 13:06:39 -0300

    Criando projeto de teste e iniciando implementação do método VerifyLatePayment

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,10 +1,30 @@
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Xunit.Abstractions;
+
 namespace Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void VerifyLatePayment()
     {
+        IExpensesRepository repository = new ExpensesRepository();
+        ExpensesEntity case1 = repository.GetById(15);
 
+        IPaidInstallmentsRepository _paidInstallmentsRepository = new PaidInstallmentsRepository(); 
+        var paidInstallments = _paidInstallmentsRepository.GetAllPaidByIdExpenses(15);
+
+        bool isSameName = case1.ExpenseName == "Pump 10" ? true : false;
+        Assert.False(isSameName, $"{case1.ExpenseName}");
+
+        string content = $"Nome: {case1.ExpenseName} \n Qtd Total: {case1.CountInstallments} \n Qtd Paga {paidInstallments.Count}";
+
+        File.WriteAllText("/Users/klebermirandalima/Projetos/teste.txt", content);
+
+        // Olhar para a última data de pagamento 
+        // Olhar para o mês atual 
+        // Olha para a data de assinatura e inclusive trocar o DateFirstInstallments do ExpenseEntity para DateSubscription
+        
     }
 }
