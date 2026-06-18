e7a6504dce5f1a88e2a7e68511634c546d84a5ac
Kleber Miranda Lima <klebermirandalima@gmail.com>
2026-06-17 23:35:29 -0300

    Implementando lógica para publicação local.

--- StorageContextJson/MachineExplorer.cs
+++ StorageContextJson/MachineExplorer.cs
@@ -4,23 +4,30 @@ public static class MachineExplorer
 {
     public static string GetFilePath(string nameRepository)
     {
-
         nameRepository = "tb_" + nameRepository;
-        
         string file = "";
 
         if (OperatingSystem.IsMacOS())
-            file = @"/" + nameRepository + ".json"/* "Data.json" */;
+            file = @"/" + nameRepository + ".json";
         else if (OperatingSystem.IsWindows())
             file = @"\" + nameRepository + ".json";
 
-        // var x = Environment.SystemDirectory;
-        // var x2 = Environment.CommandLine;
 
         string environment = Environment.CurrentDirectory;
-        // var z = typeof(EndOfStreamException ).Assembly.FullName;  
+
+        // Em caso normal
+        string replaceProject = OperatingSystem.IsWindows() ?
+            environment.Replace(@"\Api", @"\StorageContextJson") :
+            environment.Replace("/Api", "/StorageContextJson");
+
+        // Em caso de testes
         // string replaceProject = environment.Replace("/bin/Debug/net7.0", ""); /* TestProject */
-        string replaceProject = environment.Replace("Api", "StorageContextJson");
+
+        // Em caso de publicação 
+        replaceProject = OperatingSystem.IsWindows() ?
+            replaceProject.Replace(@"\publish", @"\StorageContextJson") :
+            replaceProject.Replace("/publish", "/StorageContextJson");
+
         string fullPath = replaceProject + file;
 
         return fullPath;
